fbb11b2b19703d9aceda4f2ccb6c2824e6006976
AdakHaddad <muhammadmuqtada@gmail.com>
2024-02-07 21:11:54 +0700

    Cleaning

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,14 @@
 "use client";
-import React, { useState } from "react";
-import Navbar from "@/components/Nav"; // Check the path for Navbar
-import { Login } from "@/components/Login"; // Check the path for Login
-import { PenjualanInput } from "@/components/PenjualanInput"; // Check the path for PenjualanInput
+import Navbar from "@/components/Nav";
+import { Login } from "@/components/Login";
+import { PenjualanInput } from "@/components/PenjualanInput";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
-      <Login />
+      {/* <Login /> */}
+      {/* <PenjualanInput/> */}
     </div>
   );
 };
